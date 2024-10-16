fc3f8166c49febec7b627e4d161b0cfd0654adbc
Rashad DuPaty <radupaty@gmail.com>
2024-10-16 12:10:08 +0900

    updated responsive design

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -25,3 +25,13 @@ document.addEventListener('DOMContentLoaded', () => {
     ease: 'power2.out'
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.getElementById('menu-btn');
+  const navLinks = document.getElementById('nav-links');
+
+  menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('open'); // Toggle the open class to show/hide the links
+  });
+});
+
